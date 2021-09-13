25bbf2e89c6daf2d7545f49ce59d55df316c92dd
Raynia <46100945+Raynia@users.noreply.github.com>
2021-09-13 14:24:56 +0900

    First commit of UnrealRPG Project

---
+++ README.md
@@ -0,0 +1,5 @@
+# UnrealRPG
+
+Developed with
+Unreal Engine 4,
+Visual Studio 2019
---
+++ Source/UnrealRPG/UnrealRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALRPG_API AUnrealRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
